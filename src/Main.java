a9364792835f219402b7b266b2e7cc2339ae10af
Monk <elmonk.drb@yandex.ru>
2023-11-24 21:55:20 +0300

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -14,44 +14,38 @@ public class Main {
                 throw new Exception();
             }
 
-            for (double i = 0.5; i < 100; i += 0.5) {
-                int year = currentYear;
-
-                double capital = Costants.MOEX_RATE[year - 2002];             // капитал
-
-                double expenses = capital * i / 100;        // расходы (изъятие)
-                capital = capital - expenses;
-
-                if (year > 2002) {
-                    capital = capital * Costants.MOEX_RATE[year - 2002] / Costants.MOEX_RATE[year - 2003];
-                }
-                expenses = expenses * (1 + Costants.INFLATION_RATE[year - 2002]/100);  //индекация расходов (изъятия)
-                year++;
-
-
-                while (year <= 2022) {
-
-                    capital = capital - expenses; // остаток капитала после изъятия
-
-                    capital = capital * Costants.MOEX_RATE[year - 2002] / Costants.MOEX_RATE[year - 2003]; //"прирост" капитала относительно конца и начала года
-
-                    expenses = expenses * (1 + Costants.INFLATION_RATE[year - 2002]/100);       //индексация расходов
-
-                    year++;
-                }
-
-                if (capital - expenses < 0) {
-                    maxPercent = i;
-
-                    break;
-                }
+            for (double i = 0.5; i <= 100; i += 0.5) {
+                definition(currentYear, i);
             }
 
             System.out.println(maxPercent);
 
         } catch (Exception e) {
-            System.out.println("xaxa... не правильный год)");
-            e.printStackTrace();
+            System.out.println("Введена неправильная дата");
+        }
+    }
+
+    public static void definition(int year, double i) {
+        double capital = Costants.MOEX_RATE[year - 2002];             // капитал
+        double expenses = capital * i / 100;        // расходы (изъятие)
+
+        capital = capital - expenses;
+
+        if (year > 2002) {
+            capital = capital * Costants.MOEX_RATE[year - 2002] / Costants.MOEX_RATE[year - 2003];
+        }
+        expenses = expenses * (1 + Costants.INFLATION_RATE[year - 2002] / 100);  //индекация расходов (изъятия)
+        year++;
+
+        while (year <= 2022) {
+            capital = capital - expenses; // остаток капитала после изъятия
+            capital = capital * Costants.MOEX_RATE[year - 2002] / Costants.MOEX_RATE[year - 2003]; //"прирост" капитала относительно конца и начала года
+            expenses = expenses * (1 + Costants.INFLATION_RATE[year - 2002] / 100);       //индексация расходов
+            year++;
+        }
+
+        if (capital - expenses < 0 && capital > 0) {
+            maxPercent = i;
         }
     }
 }
